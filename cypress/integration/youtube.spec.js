e4c121bf9ec0bc25354c6cfc118da14dbe060fff
Mahesh <venkatamahesh.talanki@tesco.com>
2022-03-10 21:45:49 +0530

    removed pause time to test

--- cypress/integration/youtube.spec.js
+++ cypress/integration/youtube.spec.js
@@ -2,14 +2,22 @@
 it('youtest', function() {
   /* ==== Generated with Cypress Studio ==== */
   cy.visit('https://www.youtube.com/channel/UCYENu_O0VXQ_a2YH88CKyAg');
+  cy.log('loaded page');
   cy.get(':nth-child(6) > .tab-content').click()
+  cy.log('clicked on playlist tab');
   cy.get(':nth-child(2) > h3.style-scope > #video-title').click();
   cy.get(':nth-child(2) > .yt-simple-endpoint > .style-grey-text > #button > .ytd-toggle-button-renderer').click();
+  cy.log('clicked on playlist item');
   cy.get('.ytp-mute-button').click();
+  cy.log('clicked on mute');
   cy.get('.ytp-settings-button').click();
+  cy.log('clicked on settings toggle in');
   cy.get(':nth-child(2) > .ytp-menuitem-content').click();
   cy.get(':nth-child(8) > .ytp-menuitem-label').click();
-  cy.pause(15 * 60 * 1000);
-  cy.wait(15 * 60 * 1000);
+  cy.log('clicked on fast x2');
+  cy.get('.ytp-settings-button').click();
+  cy.log('clicked on settings toggle out');
+  // cy.pause(15 * 60 * 1000);
+  // cy.wait(15 * 60 * 1000);
   /* ==== End Cypress Studio ==== */
 });
