a63e9fcc081b95616cc8ea343ab40a1f362c703b
Mahesh <venkatamahesh.talanki@tesco.com>
2022-03-11 12:00:53 +0530

    removed shuffle

--- cypress/integration/youtube.spec.js
+++ cypress/integration/youtube.spec.js
@@ -7,7 +7,7 @@ it('youtest', function () {
   cy.get(':nth-child(6) > .tab-content').click()
   cy.task('log', 'clicked on playlist tab');
   cy.get(':nth-child(2) > h3.style-scope > #video-title').click();
-  cy.get(':nth-child(2) > .yt-simple-endpoint > .style-grey-text > #button > .ytd-toggle-button-renderer').click();
+  // cy.get(':nth-child(2) > .yt-simple-endpoint > .style-grey-text > #button > .ytd-toggle-button-renderer').click();
   cy.task('log', 'clicked on playlist item');
   cy.get('.ytp-mute-button').click();
   cy.task('log', 'clicked on mute');
